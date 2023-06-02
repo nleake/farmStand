677baf564936498910f580f351d79cadcdc66abd
nleake <6044084+nleake@users.noreply.github.com>
2023-06-02 13:47:14 -0400

    initial commit, ignore node_modules

---
+++ index.js
@@ -0,0 +1,50 @@
+// Don't forget to start your MongoDB server
+//brew services start mongodb-community@6.0
+
+// Stop server with this:
+//brew services stop mongodb-community@6.0
+
+//pull up mongo shell with: mongosh
+
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(() => {
+    console.log('mongo connection open');
+})
+.catch(err => {
+    console.log('oh no, mongo connection error')
+    console.log(err)
+})
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.get('/products', async (req, res) => {
+    const products = await Product.find({})
+    res.render('products/index', { products })
+})
+
+app.get('products/new', async (req, res) => {
+    res.render('products/new')
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.send('making your product!')
+})
+
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/show', { product })
+})
+
+app.listen(3000, () => {
+    console.log('app is listening on port 3000');
+})
